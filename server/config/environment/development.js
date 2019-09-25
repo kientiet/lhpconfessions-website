2778b38521d69502a334f3022cf8a4ebc41d628c
Tiet Hao Kien <timothytiet@gmail.com>
2019-09-24 19:59:03 -0700

    Update security problem

--- server/config/environment/development.js
+++
@@ -1,7 +0,0 @@
-export default {
-    // Put your development configuration here
-    mongodb: {
-        url: "mongodb://timothytiet:29051997kk@ds159493.mlab.com:59493/lhpconfessions",
-        port: process.env.MONGODB || 27017
-    }
-};
